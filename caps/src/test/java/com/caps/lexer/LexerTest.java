50717433b7492d5ce74c882bc418d04f7a48ea61
Jasmine Maquindang <jazzix3@gmail.com>
2023-03-22 11:32:19 -0700

    lexer test updates

--- caps/src/test/java/com/caps/lexer/LexerTest.java
+++ caps/src/test/java/com/caps/lexer/LexerTest.java
@@ -84,5 +84,108 @@ public class LexerTest {
         assertArrayEquals(tokens,expected);
     }
 
+    @Test
+    public void testTokenizeWhile() throws TokenizerException {
+        final Token[] tokens = Tokenizer.tokenize("WHILE (x IS 2) {x++}");
+        final Token[] expected = new Token[]{
+                new WhileToken(),
+                new LeftParenToken(),
+                new IdentifierToken("x"),
+                new IsToken(),
+                new IntToken(2),
+                new RightParenToken(),
+                new LeftBracketToken(),
+                new IdentifierToken("x"),
+                new PlusToken(),
+                new PlusToken(),
+                new RightBracketToken()
+        };
+        assertArrayEquals(tokens,expected);
+    }
+
+    @Test
+    public void testTokenizeMethodDef() throws TokenizerException {
+        final Token[] tokens = Tokenizer.tokenize("DEFINE NUMBER addnum(NUMBER x) {sum IS x + x; RETURNS sum;}");
+        final Token[] expected = new Token[]{
+                new DefineToken(),
+                new NumberToken(),
+                new IdentifierToken("addnum"),
+                new LeftParenToken(),
+                new NumberToken(),
+                new IdentifierToken("x"),
+                new RightParenToken(),
+                new LeftBracketToken(),
+                new IdentifierToken("sum"),
+                new IsToken(),
+                new IdentifierToken("x"),
+                new PlusToken(),
+                new IdentifierToken("x"),
+                new SemicolonToken(),
+                new ReturnsToken(),
+                new IdentifierToken("sum"),
+                new SemicolonToken(),
+                new RightBracketToken()
+        };
+        assertArrayEquals(tokens,expected);
+    }
+    @Test
+    public void testTokenizeBooleanMethodDef() throws TokenizerException {
+        final Token[] tokens = Tokenizer.tokenize("DEFINE BOOLEAN isEven (NUMBER x) {RETURNS x / 2 = 0);} ");
+        final Token[] expected = new Token[]{
+                new DefineToken(),
+                new BooleanToken(),
+                new IdentifierToken("isEven"),
+                new LeftParenToken(),
+                new NumberToken(),
+                new IdentifierToken("x"),
+                new LeftBracketToken(),
+                new ReturnsToken(),
+                new IdentifierToken("x"),
+                new FowardSlashToken(),
+                new IntToken(2),
+                new EqualsToken(),
+                new IntToken(0),
+                new RightParenToken(),
+                new SemicolonToken(),
+                new RightBracketToken()
+        };
+        assertArrayEquals(tokens,expected);
+    }
+
+    @Test
+    public void testTokenizeHigherOrderFuncDef() throws TokenizerException {
+        final Token[] tokens = Tokenizer.tokenize("(x,y) EXECUTES x + y");
+        final Token[] expected = new Token[]{
+                new LeftParenToken(),
+                new IdentifierToken("x"),
+                new CommaToken(),
+                new IdentifierToken("y"),
+                new RightParenToken(),
+                new ExecutesToken(),
+                new IdentifierToken("x"),
+                new PlusToken(),
+                new IdentifierToken("y"),
+                new RightParenToken()
+        };
+        assertArrayEquals(tokens,expected);
+    }
+
+
+    public void testTokenizePrint() throws TokenizerException {
+        final Token[] tokens = Tokenizer.tokenize("x IS foo; PRINT (x);");
+        final Token[] expected = new Token[]{
+                new IdentifierToken("x"),
+                new IsToken(),
+                new StringToken("foo"),
+                new SemicolonToken(),
+                new PrintToken(),
+                new LeftParenToken(),
+                new IdentifierToken("x"),
+                new RightParenToken(),
+                new SemicolonToken()
+        };
+        assertArrayEquals(tokens,expected);
+    }
+
 }
 
