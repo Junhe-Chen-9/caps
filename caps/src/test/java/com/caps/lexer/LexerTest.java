6a32f3ea400e341991a1a294c35d66f9c80d4887
Jasmine Maquindang <jazzix3@gmail.com>
2023-03-22 12:09:01 -0700

    lexer test updates

--- caps/src/test/java/com/caps/lexer/LexerTest.java
+++ caps/src/test/java/com/caps/lexer/LexerTest.java
@@ -16,31 +16,31 @@ public class LexerTest {
     @Test
     public void testTokenizeIdentifier() throws TokenizerException {
         final Token[] tokens = Tokenizer.tokenize("bar");
-        final Token[] expeted = new Token[]{new IdentifierToken("bar")};
-        assertArrayEquals(tokens,expeted);
+        final Token[] expected = new Token[]{new IdentifierToken("bar")};
+        assertArrayEquals(tokens,expected);
     }
 
     @Test
     public void testTokenizeExp() throws TokenizerException {
         final Token[] tokens = Tokenizer.tokenize("(1 + 2)");
-        final Token[] expeted = new Token[]{
+        final Token[] expected = new Token[]{
                 new LeftParenToken(),
                 new IntToken(1),
                 new PlusToken(),
                 new IntToken(2),
                 new RightParenToken()
         };
-        assertArrayEquals(tokens,expeted);
+        assertArrayEquals(tokens,expected);
     }
     @Test
     public void testTokenizeStmt() throws TokenizerException {
         final Token[] tokens = Tokenizer.tokenize("s IS stringvalue");
-        final Token[] expeted = new Token[]{
+        final Token[] expected = new Token[]{
                 new IdentifierToken("s"),
                 new IsToken(),
                 new StringToken("stringvalue")
         };
-        assertArrayEquals(tokens,expeted);
+        assertArrayEquals(tokens,expected);
     }
 
 
@@ -80,6 +80,110 @@ public class LexerTest {
                 new ReturnsToken(),
                 new IntToken(7),
                 new RightBracketToken()
+        };
+        assertArrayEquals(tokens,expected);
+    }
+
+    @Test
+    public void testTokenizeWhile() throws TokenizerException {
+        final Token[] tokens = Tokenizer.tokenize("WHILE (x IS 2) {x++}");
+        final Token[] expected = new Token[]{
+                new WhileToken(),
+                new LeftParenToken(),
+                new IdentifierToken("x"),
+                new IsToken(),
+                new IntToken(2),
+                new RightParenToken(),
+                new LeftBracketToken(),
+                new IdentifierToken("x"),
+                new PlusToken(),
+                new PlusToken(),
+                new RightBracketToken()
+        };
+        assertArrayEquals(tokens,expected);
+    }
+
+    @Test
+    public void testTokenizeMethodDef() throws TokenizerException {
+        final Token[] tokens = Tokenizer.tokenize("DEFINE NUMBER addnum(NUMBER x) {sum IS x + x; RETURNS sum;}");
+        final Token[] expected = new Token[]{
+                new DefineToken(),
+                new NumberToken(),
+                new IdentifierToken("addnum"),
+                new LeftParenToken(),
+                new NumberToken(),
+                new IdentifierToken("x"),
+                new RightParenToken(),
+                new LeftBracketToken(),
+                new IdentifierToken("sum"),
+                new IsToken(),
+                new IdentifierToken("x"),
+                new PlusToken(),
+                new IdentifierToken("x"),
+                new SemicolonToken(),
+                new ReturnsToken(),
+                new IdentifierToken("sum"),
+                new SemicolonToken(),
+                new RightBracketToken()
+        };
+        assertArrayEquals(tokens,expected);
+    }
+    @Test
+    public void testTokenizeBooleanMethodDef() throws TokenizerException {
+        final Token[] tokens = Tokenizer.tokenize("DEFINE BOOLEAN isEven (NUMBER x) {RETURNS (x / 2) IS 0;} ");
+        final Token[] expected = new Token[]{
+                new DefineToken(),
+                new BooleanToken(),
+                new IdentifierToken("isEven"),
+                new LeftParenToken(),
+                new NumberToken(),
+                new IdentifierToken("x"),
+                new RightParenToken(),
+                new LeftBracketToken(),
+                new ReturnsToken(),
+                new LeftParenToken(),
+                new IdentifierToken("x"),
+                new FowardSlashToken(),
+                new IntToken(2),
+                new RightParenToken(),
+                new IsToken(),
+                new IntToken(0),
+                new SemicolonToken(),
+                new RightBracketToken()
+        };
+        assertArrayEquals(tokens,expected);
+    }
+
+    @Test
+    public void testTokenizeHigherOrderFuncDef() throws TokenizerException {
+        final Token[] tokens = Tokenizer.tokenize("(x,y) EXECUTES x + y");
+        final Token[] expected = new Token[]{
+                new LeftParenToken(),
+                new IdentifierToken("x"),
+                new CommaToken(),
+                new IdentifierToken("y"),
+                new RightParenToken(),
+                new ExecutesToken(),
+                new IdentifierToken("x"),
+                new PlusToken(),
+                new IdentifierToken("y"),
+        };
+        assertArrayEquals(tokens,expected);
+    }
+
+    @Test
+    public void testTokenizePrint() throws TokenizerException {
+        final Token[] tokens = Tokenizer.tokenize("x IS foo; PRINT (x);");
+        final Token[] expected = new Token[]{
+                new IdentifierToken("x"),
+                new IsToken(),
+                new StringToken("foo"),
+                new SemicolonToken(),
+                new PrintToken(),
+                new LeftParenToken(),
+                new IdentifierToken("x"),
+                new RightParenToken(),
+                new SemicolonToken()
         };
         assertArrayEquals(tokens,expected);
     }
