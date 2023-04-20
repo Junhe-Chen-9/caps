066d02f9163fe8ad0eefaa94c6fde3bafbd8428c
Jasmine Maquindang <jazzix3@gmail.com>
2023-04-20 15:21:54 -0700

    removed global p, change return to get next p

--- caps/src/main/java/com/caps/parser/Parser.java
+++ caps/src/main/java/com/caps/parser/Parser.java
@@ -4,15 +4,13 @@ import com.caps.lexer.*;
 
 public class Parser {
     private final  Token[] tokens;
-    private int p;
 
-    public Parser(final Token[] tokens, final int p){
+    public Parser(final Token[] tokens){
         this.tokens = tokens;
-        this.p = p;
     }
 
     private Token getToken(final int p) throws ParseException{
-        if(p >= 0 && p < tokens.length){
+        if(p >= 0){
             // valid position
             return tokens[p];
         }else{
@@ -28,26 +26,53 @@ public class Parser {
     }
 
     // type::= `STRING` | `NUMBER` | `BOOLEAN`
-    // todo: should this return Number instead of int?
-    public ParseResult<Type> parseType(int p) throws ParseException {
+    public ParseResult<Type> parseType(final int p) throws ParseException {
         final Token token = getToken(p);
 
-        if(p < tokens.length && token instanceof IntToken){
-            p ++;
-            return new ParseResult<>(new IntType(), p);
+        if(token instanceof NumberToken){
+            return new ParseResult<>(new NumberType(), p + 1);
         }
-        else if(p < tokens.length  && token instanceof BooleanToken){
-            p ++;
-            return new ParseResult<>(new BoolType(), p);
+        else if(token instanceof BooleanToken){
+            return new ParseResult<>(new BoolType(), p + 1);
         }
-        else if(p < tokens.length  && token instanceof StringToken) {
-            p++;
-            return new ParseResult<>(new StrType(), p);
+        else if(token instanceof StringToken) {
+            return new ParseResult<>(new StrType(), p + 1);
         }
         else {
             throw new ParseException("Expected type; received: " + token);
         }
     }
+
+    /* Example from meeting with prof
+    exps:=[exp(`,`exp)*]
+    exp::= IDENTIFIER | IDENTIFIER `(` exps `)`
+    stmt ::= `while` `(` exp `)` `{` stmt* `}` |
+        `if `(` exp `)` stmt `else` stmt |
+        IDENTIFIER `=` exp `;` |
+        exp `;`
+
+    public ParseResult<Stmt> parseStmt(final int p) throws ParseException {
+        final Token token getToken(p)
+
+        try{
+            assertTokenIs(getToken(p), new WhileToken());
+            assertTokenIs(getToken(p +1), new LeftParenToken());
+            final ParseResult<Exp> guard = parseExp(p + 2);
+            ...
+            return new ParseResult<Stmt>(new WhileStmt(...), ...);
+        }
+        catch (final ParseException e) {... }
+        try {
+            if (token instance of identifierToken){
+                assertTokenIs(getToken(p + 1), new EqualsToken());
+            }
+        }
+        catch (final ParseException e) {...}
+    }
+     */
+
+
+
 
 
     public ParseResult<Op> parseOp (final int p) throws ParseException {
@@ -67,7 +92,7 @@ public class Parser {
         }
 
         assert(op != null);
-        return new ParseResult<Op>(op, p + 1);
+        return new ParseResult<Op>(op, p);
 
     }
 
